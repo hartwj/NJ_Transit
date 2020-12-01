71292460cd25dee68b509dbfc4c73e768f366a7a
hartwj <70765227+hartwj@users.noreply.github.com>
2020-11-30 22:07:54 -0500

    Update final_proj.R

--- Raw_Data/final_proj.R
+++ Raw_Data/final_proj.R
@@ -7,7 +7,7 @@
 options(scipen=999)
 
 ## Download + Load Packages
-pckgs <- c("tidyverse", "kableExtra", "readr", "ggplot2", "lubridate")
+pckgs <- c("tidyverse", "kableExtra", "readr", "ggplot2", "lubridate","sf")
 
 if (any(pckgs %notin% rownames(installed.packages())==TRUE)){
   install.packages(pckgs, repos = c(CRAN = "http://cloud.r-project.org"))}
@@ -25,7 +25,8 @@ data$delay_binary <- ifelse(data$delay_minutes >5,1,0)
 
 ## NJ Transit only
 njtransit <- data %>%
-  filter(type == "NJ Transit")
+  filter(type == "NJ Transit") %>%
+  mutate(id = rownames(.))
 
 ##Build hour,day,week,month columns
 #ignoring year - there's only two (2018 & 2019 and I want random test/train splits)
@@ -50,14 +51,62 @@ njtransit <- njtransit %>%
           rush = ifelse(hour<10 & hour>=6 & weekday=="Yes","Yes",
                        ifelse(hour<20 & hour>=16 & weekday=="Yes","Yes","No")))
 
-#rowID and characters as factors
-njtransit <- njtransit %>%
-  mutate_if(., is.character, as.factor) %>%
-  mutate(id = rownames(.))
-
-
 #Origin & Destination Feature
+
+njtransit <- njtransit %>%
+  group_by(train_id) %>%
+  arrange(stop_sequence) %>%
+  filter(row_number()==1) %>%
+  mutate(origin = to) %>%
+  select(train_id,origin) %>%
+  right_join(njtransit,.,by="train_id") %>%
+  group_by(train_id) %>%
+  arrange(stop_sequence) %>%
+  filter(row_number()==n()) %>%
+  mutate(destination = to) %>%
+  select(train_id,origin,destination) %>%
+  right_join(njtransit,.,by="train_id")
+
 #Includes Manhattan
+
+njtransit <- njtransit %>%
+  mutate(manhattan = ifelse(origin == "New York Penn Station","Yes",ifelse(destination == "New York Penn Station","Yes","No")))
+
+#characters as factors
+njtransit <- njtransit %>%
+  mutate_if(., is.character, as.factor) 
+
+
+### station geometries
+### used long island ESRI which is recommended for NYC
+
+station_geo <- st_read("https://opendata.arcgis.com/datasets/acf1aa71053f4bb48a1aad7034f35e48_3.geojson") %>%
+  st_transform('ESRI:102318')
+
+
+##Prepping for merge
+
+station_geo$ATIS_ID <- gsub("RAIL","",station_geo$ATIS_ID) 
+  
+station_geo$ATIS_ID <- sub("^0+", "", station_geo$ATIS_ID)
+
+
+## merge
+njtransit$to_id <- as.character(njtransit$to_id)
+station_geo$to_id <- station_geo$ATIS_ID 
+
+#breaks at st_as_sf because tons of missing lat and long because these freaking IDs aren't 100% the same
+njtransit_sf <- station_geo %>%
+  st_drop_geometry() %>%
+  select(LATITUDE, LONGITUDE,STATION,to_id) %>%
+  plyr::join(njtransit,.,by="to_id",type="left",match="first") %>%
+  st_as_sf(coords = c("LONGITUDE","LATITUDE"), crs=4326, agr = "constant") %>%
+  st_transform('ESRI:102318')
+
+
+
+
+
 
 
 
