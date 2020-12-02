3d6c575737abf99007afb9daa08fdb15c78bdb4d
zhoujuliana <56312807+zhoujuliana@users.noreply.github.com>
2020-12-01 19:23:53 -0500

    Weather data

--- Raw_Data/final_proj.R
+++ Raw_Data/final_proj.R
@@ -1,5 +1,6 @@
 ### NJ Transit - MUSA Final Project
 
+# --- Setup: Libraries ----
 # create notin operator
 `%notin%` <- Negate(`%in%`)
 
@@ -7,7 +8,8 @@
 options(scipen=999)
 
 ## Download + Load Packages
-pckgs <- c("tidyverse", "kableExtra", "readr", "ggplot2", "lubridate","sf")
+pckgs <- c("tidyverse", "kableExtra", "readr", "ggplot2", "lubridate","sf",
+           "riem","tigris", "gganimate","gridExtra","knitr")
 
 if (any(pckgs %notin% rownames(installed.packages())==TRUE)){
   install.packages(pckgs, repos = c(CRAN = "http://cloud.r-project.org"))}
@@ -15,6 +17,82 @@ if (any(pckgs %notin% rownames(installed.packages())==TRUE)){
 lapply(pckgs, FUN = require, character.only = TRUE)
 
 
+# --- Setup: Aesthetics & Functions ----
+mapTheme <- function(base_size = 12) {
+  theme(
+    text = element_text( color = "black"),
+    plot.title = element_text(size = 14,colour = "black"),
+    plot.subtitle=element_text(face="italic"),
+    plot.caption=element_text(hjust=0),
+    axis.ticks = element_blank(),
+    panel.background = element_blank(),axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_rect(colour = "black", fill=NA, size=2)
+  )
+}
+
+plotTheme <- function(base_size = 12) {
+  theme(
+    text = element_text( color = "black"),
+    plot.title = element_text(size = 14,colour = "black"),
+    plot.subtitle = element_text(face="italic"),
+    plot.caption = element_text(hjust=0),
+    axis.ticks = element_blank(),
+    panel.background = element_blank(),
+    panel.grid.major = element_line("grey80", size = 0.1),
+    panel.grid.minor = element_blank(),
+    panel.border = element_rect(colour = "black", fill=NA, size=2),
+    strip.background = element_rect(fill = "grey80", color = "white"),
+    strip.text = element_text(size=12),
+    axis.title = element_text(size=12),
+    axis.text = element_text(size=10),
+    plot.background = element_blank(),
+    legend.background = element_blank(),
+    legend.title = element_text(colour = "black", face = "italic"),
+    legend.text = element_text(colour = "black", face = "italic"),
+    strip.text.x = element_text(size = 14)
+  )
+}
+
+palette5 <- c("#25CB10", "#5AB60C", "#8FA108",   "#C48C04", "#FA7800")
+
+qBr <- function(df, variable, rnd) {
+  if (missing(rnd)) {
+    as.character(quantile(round(df[[variable]],0),
+                          c(.01,.2,.4,.6,.8), na.rm=T))
+  } else if (rnd == FALSE | rnd == F) {
+    as.character(formatC(quantile(df[[variable]]), digits = 3),
+                 c(.01,.2,.4,.6,.8), na.rm=T)
+  }
+}
+
+q5 <- function(variable) {as.factor(ntile(variable, 5))}
+
+nn_function <- function(measureFrom,measureTo,k) {
+  measureFrom_Matrix <-
+    as.matrix(measureFrom)
+  measureTo_Matrix <-
+    as.matrix(measureTo)
+  nn <-   
+    get.knnx(measureTo, measureFrom, k)$nn.dist
+  output <-
+    as.data.frame(nn) %>%
+    rownames_to_column(var = "thisPoint") %>%
+    gather(points, point_distance, V1:ncol(.)) %>%
+    arrange(as.numeric(thisPoint)) %>%
+    group_by(thisPoint) %>%
+    summarize(pointDistance = mean(point_distance)) %>%
+    arrange(as.numeric(thisPoint)) %>% 
+    dplyr::select(-thisPoint) %>%
+    pull()
+  
+  return(output)  
+}
+
+# --- Data ----
 ### Read in CSV files
 
 files <- list.files(path = "./Raw_Data/",pattern = "*.csv", full.names = T)
@@ -28,8 +106,8 @@ njtransit <- data %>%
   filter(type == "NJ Transit") %>%
   mutate(id = rownames(.))
 
-##Build hour,day,week,month columns
-#ignoring year - there's only two (2018 & 2019 and I want random test/train splits)
+## Create hour, week, day of the week, and month columns
+# Ignoring year - there's only two (2018 & 2019 and I want random test/train splits)
 njtransit <- njtransit %>%
   mutate(interval60 = floor_date(ymd_hms(scheduled_time), unit = "hour"),
          interval15 = floor_date(ymd_hms(scheduled_time), unit = "15 mins"),
@@ -38,8 +116,8 @@ njtransit <- njtransit %>%
          dotw = wday(interval60, label=TRUE),
          month = month(interval60, label=TRUE))
 
-#weekday vs. weekend
-#rush hour between 6am and 10am or 4pm and 8pm
+# Weekday vs. weekend
+# Rush hour between 6am and 10am or 4pm and 8pm
 njtransit <- njtransit %>%
   mutate(weekday = recode(dotw, "Mon" = "Yes",
                           "Tue" = "Yes",
@@ -51,8 +129,35 @@ njtransit <- njtransit %>%
           rush = ifelse(hour<10 & hour>=6 & weekday=="Yes","Yes",
                        ifelse(hour<20 & hour>=16 & weekday=="Yes","Yes","No")))
 
-#Origin & Destination Feature
+# --- Features ----
+# Weather Feature
+weather.Data <- riem_measures(station = "EWR", date_start = "2018-03-01", date_end = "2020-05-31")
 
+## Plot Newark weather data
+weather.Panel <-  
+  weather.Data %>%
+  mutate_if(is.character, list(~replace(as.character(.), is.na(.), "0"))) %>% 
+  replace(is.na(.), 0) %>%
+  mutate(interval60 = ymd_h(substr(valid, 1, 13))) %>%
+  mutate(week = week(interval60),
+         dotw = wday(interval60, label=TRUE)) %>%
+  group_by(interval60) %>%
+  summarize(Temperature = max(tmpf),
+            Precipitation = sum(p01i),
+            Wind_Speed = max(sknt)) %>%
+  mutate(Temperature = ifelse(Temperature == 0, 42, Temperature))
+
+grid.arrange(top = "Weather Data - Newark - Mar 2018 to May 2020",
+             ggplot(weather.Panel, aes(interval60,Precipitation)) + geom_line() + 
+               labs(title="Precipitation", x="Hour", y="Percipitation") + plotTheme(),
+             ggplot(weather.Panel, aes(interval60,Wind_Speed)) + geom_line() + 
+               labs(title="Wind Speed", x="Hour", y="Wind Speed") + plotTheme(),
+             ggplot(weather.Panel, aes(interval60,Temperature)) + geom_line() + 
+               labs(title="Temperature", x="Hour", y="Temperature") + plotTheme())
+
+
+
+# Origin & Destination Feature
 njtransit <- njtransit %>%
   group_by(train_id) %>%
   arrange(stop_sequence) %>%
@@ -67,29 +172,24 @@ njtransit <- njtransit %>%
   select(train_id,origin,destination) %>%
   right_join(njtransit,.,by="train_id")
 
-#Includes Manhattan
-
+# Includes Manhattan Feature
 njtransit <- njtransit %>%
   mutate(manhattan = ifelse(origin == "New York Penn Station","Yes",ifelse(destination == "New York Penn Station","Yes","No")))
 
-#characters as factors
+# Characters as factors
 njtransit <- njtransit %>%
   mutate_if(., is.character, as.factor) 
 
 
-### station geometries
-### used long island ESRI which is recommended for NYC
-
+### Station geometries
+### Used Long Island ESRI which is recommended for NYC
 station_geo <- st_read("https://opendata.arcgis.com/datasets/acf1aa71053f4bb48a1aad7034f35e48_3.geojson") %>%
   st_transform('ESRI:102318')
 
-
-##Prepping for merge
+## Prepping for merge
 
 station_geo$ATIS_ID <- gsub("RAIL","",station_geo$ATIS_ID) 
-  
 station_geo$ATIS_ID <- sub("^0+", "", station_geo$ATIS_ID)
-
 
 ## merge
 njtransit$to_id <- as.character(njtransit$to_id)
